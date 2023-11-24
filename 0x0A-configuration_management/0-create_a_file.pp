d6b636c2c95c492e7de6f10033af13ba9c881926
Smilecornerstone <adelekekayodesamuel@gmail.com>
2023-11-24 10:14:39 -0800

    0x0A. Configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a file in the /tmp directory
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
